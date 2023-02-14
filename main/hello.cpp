e3a416aaaff7ed424a6f5ba9ed7c58356caf8045
tanishq4142 <78026284+tanishq4142@users.noreply.github.com>
2023-02-14 13:45:51 +0530

    Second commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "This is the new Statement added by me (Tanishq Chugh -- PES1UG20CS460)";
   return 0;
 }
