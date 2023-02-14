b80fb15b3bb22267d29e923ae2a79df9504666b3
tanishq4142 <78026284+tanishq4142@users.noreply.github.com>
2023-02-14 15:16:05 +0530

    Update PES1UG20CS460-1.cpp

--- PES1UG20CS460-1.cpp
+++ PES1UG20CS460-1.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 
 int main(void) {
-  cout << "This is the new CPP file created by Tanishq-Chugh (PES1UG20CS460)\n";
+  cot << "This is the new CPP file created by Tanishq-Chugh (PES1UG20CS460)\n";
   return 0;
 }
